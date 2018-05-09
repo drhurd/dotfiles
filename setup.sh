3ded43dcf65bdcb99314317221b98c9907c01d52
dhurd <dylan.hurd@airbnb.com>
2018-05-09 10:33:00 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #
+# HELPER FUNCTIONS
+# ------------------------------
+# Defines helper functions to be used later on
+# brew_if runs brew install [program] if [program] is not installed
+function brew_if {
+  if [ -z `which $1` ]; then
+    brew install $1
+  fi
+}
+
+#
 # SYMLINK DOTFILES
-# ------------
+# ------------------------------
 # Links all dotfiles into home directory
 ln ~/.dotfiles/aliases ~/.aliases
 ln ~/.dotfiles/bash_profile ~/.bash_profile
@@ -11,5 +22,22 @@ ln ~/.dotfiles/vimrc ~/.vimrc
 
 #
 # VIM SETUP
-# ------------
+# ------------------------------
 # Links all dotfiles into home directory
+
+# TODO
+
+#
+# UTILITIES
+# ------------------------------
+# Installs a list of utilities that I consider fairly essential
+
+# homebrew
+if [[ -z `which brew` && `uname -s` == 'Darwin' ]]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# other utilities
+brew_if tmux
+brew_if ag
+
